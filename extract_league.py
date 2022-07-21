47af7befbcd28342caea2a439047d7e7b7da9716
hilsts <hilstah@gmail.com>
2022-07-20 22:50:47 -0300

    airflow 1

--- extract_league.py
+++ extract_league.py
@@ -2,30 +2,28 @@ from BSW_TFT.routines.full_region_league import extract_league
 from airflow.operators.python import PythonOperator
 from airflow import DAG
 from datetime import datetime, timedelta
+from airflow.providers.mongo.hooks.mongo import MongoHook
 
+def extract(**context):
+    data = extract_league()
+    t1.xcom_push(key="extract_league", value=data, context=context)
+
+def save_to_mongo(**context):
+
+    data = t2.xcom_pull(key="extract_league", context=context)
+    print(data)
+    mongo = MongoHook('mongo')
+    for i in data:
+        mongo.insert_many(mongo_collection='league', docs=i)
 
 with DAG(
-    'extract_league',
-    # These args will get passed on to each operator
-    # You can override them on a per-task basis during operator initialization
+    dag_id='extract_league',
     default_args={
         'depends_on_past': False,
         'email_on_failure': False,
         'email_on_retry': False,
         'retries': 1,
         'retry_delay': timedelta(minutes=5),
-        # 'queue': 'bash_queue',
-        # 'pool': 'backfill',
-        # 'priority_weight': 10,
-        # 'end_date': datetime(2016, 1, 1),
-        # 'wait_for_downstream': False,
-        # 'sla': timedelta(hours=2),
-        # 'execution_timeout': timedelta(seconds=300),
-        # 'on_failure_callback': some_function,
-        # 'on_success_callback': some_other_function,
-        # 'on_retry_callback': another_function,
-        # 'sla_miss_callback': yet_another_function,
-        # 'trigger_rule': 'all_success'
     },
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022, 6, 6),
@@ -35,7 +33,13 @@ with DAG(
 
     t1 = PythonOperator(
         task_id='api_get',
-        python_callable=extract_league
+        provide_context=True,
+        python_callable=extract
     )
 
-t1
+    t2 = PythonOperator(
+        task_id='mongo_save',
+        python_callable=save_to_mongo
+    )
+
+t1 >> t2
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 
 from datetime import datetime, timedelta
 from textwrap import dedent
-
+from airflow.providers.mongo.hooks.mongo import MongoHook
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
 
